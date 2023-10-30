7a1528bc9146635c0d83416c177405c3cd0907f7
simran019 <simran.19.1677@gmail.com>
2023-10-30 14:10:58 +0530

    added login functionality

--- app/Login/login.tsx
+++ app/Login/login.tsx
@@ -1,9 +1,40 @@
+"use client";
+
+import {useState} from "react";
+import Image from "next/image";
+
 import UsernameIcon from "../../public/images/icons8-username-50.png";
 import PasswordIcon from "../../public/images/icons8-password-50 (1).png";
-import Image from "next/image";
-const Login = () => {
 
-  return (
+const Login = (props:any) => {
+    
+    const [username, setUsername] = useState({
+        value:"",
+        validity:false
+    });
+    
+    const [password, setPassword] = useState({
+        value:"",
+        validity:false
+    });
+
+    const [isSubmit, setIsSubmit] = useState(false);
+
+
+    const inputHandler =(identifier:string,value:string)=>{
+        if(identifier=="email"){
+            setUsername({value:value,validity:value.includes("@") && value.includes(".")})
+        }
+        else if(identifier=="password"){
+            setPassword({value:value,validity:value.trim().length>6})
+        }
+    }
+
+    const submitHandler =()=>{
+        setIsSubmit(true);
+        props.onLogin(true);
+    }
+    return (
     <div className="text-black w-1/3 shadow-xl shadow-slate-500 flex flex-col items-center gap-8 px-2 py-4">
       <p className="text-5xl font-extrabold text-orange-500">LOGIN</p>
       <div className="flex flex-col gap-4">
@@ -18,8 +49,10 @@ const Login = () => {
           <input
             className="rounded-md p-2 border-2 border-orange-500"
             placeholder="username"
+            onChange={(event)=>inputHandler("email",event.target.value)}
           />
         </div>
+        {isSubmit && username.validity==false && <span className="text-red-800">Invalid Username</span>}
         <div className="flex gap-4 items-center">
           <Image
             src={PasswordIcon}
@@ -30,12 +63,16 @@ const Login = () => {
           <input
             className="rounded-md p-2 border-2 border-orange-500"
             placeholder="password"
+            onChange={(event)=>inputHandler("password",event.target.value)}
           />
         </div>
+        {isSubmit && password.validity==false && <span className="text-red-800">Invalid Password</span>}
       </div>
-      <button className="bg-black text-white hover:bg-orange-500 rounded-md p-2">
+      <button className="bg-black text-white focus:bg-orange-500 rounded-md p-2"
+      onClick={submitHandler}
+      >
                 Login
-            </button>
+      </button>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
 import Header from "./Header/header";
 import Login from "./Login/login";
 
 export default function Home() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler =(val:boolean)=>{
+    setIsLoggedIn(val)
+  }
   return (
     <div className="text-white w-screen h-screen flex flex-col items-center gap-10">
-      <Header/>
-      <Login/>
+      <Header hasLoggedIn={isLoggedIn} onLogin={loginHandler}/>
+      { isLoggedIn==false && <Login onLogin={loginHandler}/> }
     </div>
   )
 }
